be31bd92d08456cb677fdd063d53cf176d76a245
Fernando souza <fernandofvsouza@icloud.com>
2025-12-30 16:00:07 +0000

    metodos de texto

---
+++ Javascript/10-Metodos-de-texto/Aula49-Textos-maiusculos-e-minusculos/script.js
@@ -0,0 +1,10 @@
+//Textos em maiusculos e minusculos
+let message = "Estou estudando os fundamentos do javascript";
+console.log(message);
+
+//Exibindo em maiusculo
+console.log(message.toUpperCase());
+
+//Exibindo em minusculo
+console.log(message.toLowerCase());
+
